a2946568418c184f01e114abe1015966f3ac3842
MAmaral1303 <103151483+MAmaral1303@users.noreply.github.com>
2023-05-16 11:18:36 -0400

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,8 +8,6 @@ import { Injectable } from '@angular/core';
 export class CartService {
   items: Product[] = [];
 
-  constructor(private http: HttpClient) {}
-
   addToCart(product: Product) {
     this.items.push(product);
   }
